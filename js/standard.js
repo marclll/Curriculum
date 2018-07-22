c4384f1a3055f1a70e4e92905ef350d11684abf9
marclll <sgmarcsg@gmail.com>
2018-07-23 01:08:09 +0200

    g

--- js/standard.js
+++ js/standard.js
@@ -5,8 +5,13 @@ $().ready(function(){
 function menu_change(){
 	if ($("#links_fondo").css("display")=="none"){
 		$("#links_fondo").css("display","flex");
+		$("#menulogo").css("animation-play-state","running");
 	}
 	else{
 		$("#links_fondo").css("display","none");
+		$("#menulogo").css("animation-play-state","running");
 	}
+	setTimeout(function(){
+		$("#menulogo").css("animation-play-state","paused");
+	},200);
 }
